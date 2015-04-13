0ea6ea2688c9c110df1cb429b5f78134dae300ac
unknown <bret.maxime@gmail.com>
2015-04-13 23:09:45 +0200

    add comment

--- www/js/services/popup.js
+++ www/js/services/popup.js
@@ -4,6 +4,17 @@ angular.module('starter.services')
     .factory('popupService', popupService);
 
 popupService.$injector = ['popupService'];
+
+/**
+ * This service is a wrapper of popup. Use it instead of $ionicPopup or other as it may centralize the app logic
+ * in one place and use the same way for all the app. Useful example:
+ *  - using several libraries can be wrapped here
+ *  - add condition relative to platform
+ *  - etc
+ *
+ * @param $ionicPopup
+ * @returns {{error: Function, badCredentials: Function, loggedOut: Function}}
+ */
 function popupService($ionicPopup){
 
     return {
